fded50539ba99ed61b409e477d39fbd51f0bbe1c
Taylor <taylor.sheets3@gmail.com>
2022-09-13 19:53:02 -0700

    further reconciliation with changes

--- src/FatDancer.js
+++ src/FatDancer.js
@@ -1,18 +1,18 @@
-var makeFatDancer = function(top, left, timeBetweenSteps){
+var makeFatDancer = function(top, left, timeBetweenSteps) {
 
   // subclass of Blinky dancer
-  makeBlinkyDancer.call(this, top, left, timeBetweenSteps)
+  makeBlinkyDancer.call(this, top, left, timeBetweenSteps);
 
   // set node class to fat - dancer
-  this.$node = $('<span class="fat-dancer"></span>')
+  this.$node = $('<span class="fat-dancer"></span>');
 
   // blinks at half the speed
-  arguments[2] = arguments[2] * 2
+  arguments[2] = arguments[2] * 2;
 
   // set position
-  this.setPosition(top,left)
-}
+  this.setPosition(top, left);
+};
 
 // pseudoclassical prototype set up
-makeFatDancer.prototype = Object.create(makeBlinkyDancer.prototype)
-makeFatDancer.prototype.constructor = makeFatDancer
+makeFatDancer.prototype = Object.create(makeBlinkyDancer.prototype);
+makeFatDancer.prototype.constructor = makeFatDancer;
--- src/init.js
+++ src/init.js
@@ -5,21 +5,21 @@ $(document).ready(function() {
   $('.floatLeftButton').on('click', function(event) {
     //iterate through array
     for (var i = 0; i < window.dancers.length; i ++) {
-      let top = $("body").height() * Math.random();
+      let top = $('body').height() * Math.random();
       let left = 0;
       window.dancers[i].setPosition(top, left);
     }
-  })
+  });
 
   // add new button
   $('.floatRightButton').on('click', function(event) {
     //iterate through array
     for (var i = 0; i < window.dancers.length; i ++) {
-      let top = $("body").height() * Math.random();
-      let left = $("body").width();
+      let top = $('body').height() * Math.random();
+      let left = $('body').width();
       window.dancers[i].setPosition(top, left);
     }
-  })
+  });
 
 
 
@@ -33,36 +33,19 @@ $(document).ready(function() {
     // make a dancer with a random position
 
     var dancer = new dancerMakerFunction(
-      $("body").height() * Math.random(),
-      $("body").width() * Math.random(),
+      $('body').height() * Math.random(),
+      $('body').width() * Math.random(),
       Math.random() * 1000
     );
     $('body').append(dancer.$node);
     window.dancers.push(dancer);
   });
 
-
-<<<<<<< HEAD
-  $('.FatDancer').on('click', function(event) {
-    /* This function sets up the click handlers for the create-dancer
-    * buttons on dancefloor.html. You should only need to make one small change to it.
-    * As long as the "data-dancer-maker-function-name" attribute of a
-    * class="addDancerButton" DOM node matches one of the names of the
-    * maker functions available in the global scope, clicking that node
-    * will call the function to make the dancer.
-    */
-
-    /* dancerMakerFunctionName is a string which must match
-    * one of the dancer maker functions available in global scope.
-    * A new object of the given type will be created and added
-    * to the stage.
-    */
-
-    var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
-=======
-$('.FatDancer').on('click', function(event) {
   var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
->>>>>>> 963793294b0c0661a86422e97c2409e6e40d24e3
+
+  $('.FatDancer').on('click', function(event) {
+    var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
+
 
     // get the maker function for the kind of dancer we're supposed to make
     var dancerMakerFunction = window[dancerMakerFunctionName];
@@ -70,8 +53,8 @@ $('.FatDancer').on('click', function(event) {
     // make a dancer with a random position
 
     var dancer = new dancerMakerFunction(
-      $("body").height() * Math.random(),
-      $("body").width() * Math.random(),
+      $('body').height() * Math.random(),
+      $('body').width() * Math.random(),
       Math.random() * 1000
     );
     $('body').append(dancer.$node);
@@ -79,20 +62,6 @@ $('.FatDancer').on('click', function(event) {
   });
 
   $('.starDancerButton').on('click', function(event) {
-    /* This function sets up the click handlers for the create-dancer
-    * buttons on dancefloor.html. You should only need to make one small change to it.
-    * As long as the "data-dancer-maker-function-name" attribute of a
-    * class="addDancerButton" DOM node matches one of the names of the
-    * maker functions available in the global scope, clicking that node
-    * will call the function to make the dancer.
-    */
-
-    /* dancerMakerFunctionName is a string which must match
-    * one of the dancer maker functions available in global scope.
-    * A new object of the given type will be created and added
-    * to the stage.
-    */
-
     var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
 
     // get the maker function for the kind of dancer we're supposed to make
@@ -101,8 +70,8 @@ $('.FatDancer').on('click', function(event) {
     // make a dancer with a random position
 
     var dancer = new dancerMakerFunction(
-      $("body").height() * Math.random(),
-      $("body").width() * Math.random(),
+      $('body').height() * Math.random(),
+      $('body').width() * Math.random(),
       Math.random() * 1000
     );
     $('body').append(dancer.$node);
@@ -110,9 +79,6 @@ $('.FatDancer').on('click', function(event) {
   });
 });
 
-<<<<<<< HEAD
-
-=======
 $('.starDancerButton').on('click', function(event) {
   var dancerMakerFunctionName = $(this).data('dancer-maker-function-name');
 
@@ -122,11 +88,11 @@ $('.starDancerButton').on('click', function(event) {
   // make a dancer with a random position
 
   var dancer = new dancerMakerFunction(
-    $("body").height() * Math.random(),
-    $("body").width() * Math.random(),
+    $('body').height() * Math.random(),
+    $('body').width() * Math.random(),
     Math.random() * 1000
   );
   $('body').append(dancer.$node);
   window.dancers.push(dancer);
-  });
->>>>>>> 963793294b0c0661a86422e97c2409e6e40d24e3
+});
+
